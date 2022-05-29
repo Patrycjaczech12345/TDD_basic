9a3dfe3283811c962bc7ee9a358e6e9bcb5665dc
student <tester@tester-m>
2022-05-29 10:52:21 +0000

    dane do gry

---
+++ test.py
@@ -0,0 +1,9 @@
+from functions import *
+
+def test_1():
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(2) == 2
+    assert fizzbuzz(3) == 'fizz'
+    assert fizzbuzz(4) == 4
+    assert fizzbuzz(5) == 'buzz'
+    assert fizzbuzz(6) == 'fizz'
